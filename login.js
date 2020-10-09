b14959db6852d6ca7192384967376f82343e1396
kavya204 <71222073+kavya204@users.noreply.github.com>
2020-10-09 20:26:31 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,14 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function showalert(){
+    alert("Successfully Registered !! Lets Login");
+};
+
+function passalert(){
+    alert("You will rececive a password reset link to the given mail id. Please check your inbox");
+};
+
